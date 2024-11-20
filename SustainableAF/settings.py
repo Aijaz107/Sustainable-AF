ad2598812710c347ce032e9c84f59b2a2c3e9b03
mohdaijazahmed2000@gmail.com <mohdaijazahmed2000@gmail.com>
2024-11-19 23:54:39 -0500

    fixing CORS error

--- SustainableAF/settings.py
+++ SustainableAF/settings.py
@@ -29,6 +29,14 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS',
+]
 
 # Application definition
 
